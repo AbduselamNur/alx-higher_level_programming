6cb8eb97061aa0ba7f72ed10de295393a6d07a8c
abduselamNur <abduselamnurhussen626@gmail.com0>
2023-05-10 01:28:56 -0700

    check cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * check_cycle - check list the list of node
+ * @list: the list of lisint_t
+ * Return: 1 or  0.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *i1;
+	listint_t *i2;
+
+	i1 = list;
+	i2 = list;
+	if (list == NULL)
+	{
+		return (0);
+	}
+	i2 = i2->next;
+	while (i2 != NULL && i2->next != NULL && i1 != NULL)
+	{
+		if (i1 == i2)
+		{
+			return (1);
+		}
+		i1 = i1->next;
+		i2 = i2->next->next;
+	}
+	return (0);
+}
