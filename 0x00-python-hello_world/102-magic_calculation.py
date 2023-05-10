bc9e90dd2ebc96e09ce0e9db34391161dcad687a
abduselamNur <abduselamnurhussen626@gmail.com0>
2023-05-09 22:51:45 -0700

    advanced

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,8 +1,3 @@
+#!/usr/bin/python3
 def magic_calculation(a, b):
-  const_98 = 98
-  a_value = a
-  b_value = b
-
-  result = const_98 ** a_value + b_value
-
-  return result
+    return (98 + (a ** b))
