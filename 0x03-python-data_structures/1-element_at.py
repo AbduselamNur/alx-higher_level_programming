1073084be91394f013764aabcea3d8543736672c
abduselamNur <abduselamnurhussen626@gmail.com0>
2023-05-16 05:13:28 -0700

    index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,5 +3,5 @@ def element_at(my_list, idx):
     for i in my_list:
         if idx < 0 and idx > len(my_list):
             return None
-        else:
-            return my_list[idx]
+        elif idx == my_list.index(i):
+            return i
