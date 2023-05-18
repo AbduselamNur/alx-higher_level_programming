d07e49341cb6fe684adf7a3d6a6a73cef2a9471e
abduselamNur <abduselamnurhussen626@gmail.com0>
2023-05-17 21:33:58 -0700

    first_char_leng

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     for i in sentence:
-        if len(sentence) < 0:
+        if not sentence:
             return len(sentence), None
         leng = len(sentence)
         fir = sentence[0]
