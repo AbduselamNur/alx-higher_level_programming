59ab3f4c6d1c9ba1f97d7ff033b0b10e2b433746
abduselamNur <abduselamnurhussen626@gmail.com0>
2023-05-21 04:28:12 -0700

    common

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    com = set_1 & set_2
-    for i in com:
-        if not com:
-            return []
-        else:
-            return i
+    com = set_1.intersection(set_2)
+    return com
