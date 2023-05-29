b6e8cc7fd4e189d56be852bcf37705e2db592b39
abduselamNur <abduselamnurhussen626@gmail.com0>
2023-05-29 10:04:13 -0700

    check_int

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    if value is None:
+        return 0
+    try:
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return True
+        else:
+            return False
+    except:
+        pass
