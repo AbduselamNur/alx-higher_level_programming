a4265ff380c4b733cddf5ca8eec228780a597efd
abduselamNur <abduselamnurhussen626@gmail.com0>
2023-05-29 09:45:22 -0700

    print_list_by_x

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    if my_list == None:
+        return 0
+    leng = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            leng += 1
+        print()
+    except IndexError:
+        print()
+    return leng 
