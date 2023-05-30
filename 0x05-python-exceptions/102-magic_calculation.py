2844edf967b1cd1fd590b7edfb44a6e05efb2496
abduselamNur <abduselamnurhussen626@gmail.com0>
2023-05-30 04:59:30 -0700

    magic

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    res = 0
+
+    for i in range(1, 4):
+        if i > a:
+            raise Exception('Too far')
+
+    res = res + a ** b // i
+
+    res += b
+
+    return result
