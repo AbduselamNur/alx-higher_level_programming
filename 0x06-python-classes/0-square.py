b3036b6f86eb6efe4c58583728309cc61f8c48f2
abduselamNur <abduselamnurhussen626@gmail.com0>
2023-05-31 21:09:07 -0700

    empty_class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 class Square:
-    """Example of an empty class Square that defines a square:"""
+    """
+    Example of an empty class Square that defines a square:
+
+    Square is four side shape
+
+    This class is no Agru and Methods
+    """
+
     pass
