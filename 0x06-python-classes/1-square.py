69bb80ade5f7aa48ba8dc47d0db577a7ac892f3c
abduselamNur <abduselamnurhussen626@gmail.com0>
2023-05-31 21:40:26 -0700

    init_class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""There is No Module in this Task
+"""
+
+
+class Square:
+
+    """
+    Represent Square Class
+
+    Attributes:
+        size (int): The size of squre
+    """
+
+    def __init__(self, size):
+        """
+        Initialize The Square Class
+
+        Args:
+            size (int): the size of a squar
+        """
+
+        self.__size = size
