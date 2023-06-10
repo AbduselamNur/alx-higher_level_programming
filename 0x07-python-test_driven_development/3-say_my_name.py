f42edb4937dbfb0747ea64009dfee7cbebae5940
abduselamNur <abduselamnurhussen626@gmail.com0>
2023-06-10 12:24:09 -0700

    firstname-lastname

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -12,7 +12,7 @@ def say_my_name(first_name, last_name=""):
         first_name (str): user first name
         last_name (str): user last name default: ""
 
-    Raise: 
+    Raise:
         TypeError: first_name must be a string
                    last_name must be a string
     """
