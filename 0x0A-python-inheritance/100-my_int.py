99a376fe9fd98acd7b72a876e085c988a8f7917d
abduselamNur <abduselamnurhussen626@gmail.com0>
2023-07-01 08:07:20 -0700

    pass

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 """
-    100-my_int: class MyInt
+MyInt class module.
+
+
 """
 
 
 class MyInt(int):
-    """
-        MyInt implements int. (inherits)
-    """
-    def __init__(self, number):
-        self.number = number
+    """Define MyInt."""
 
-    def __n__(self, value):
-        return (self.number == value)
+    def __eq__(self, value):
+        return super().__ne__(value)
 
-    def __e__(self, value):
-        return (self.number != value)
-
-    def __s__(self):
-        return (str(self.number))
+    def __ne__(self, value):
+        return super().__eq__(value)
