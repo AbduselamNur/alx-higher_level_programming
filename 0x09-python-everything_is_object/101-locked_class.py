7b71a7b159bd1d3fcb52d862dc5de8c700644e0b
abduselamNur <abduselamnurhussen626@gmail.com0>
2023-07-01 05:01:23 -0700

    last

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    creat LockedClass
+"""
+
+
+class LockedClass:
+    """
+        A class that  one attribute first_name.
+        Attribute:
+             first_name (str): name
+    """
+    __slots__ = ['first_name']
