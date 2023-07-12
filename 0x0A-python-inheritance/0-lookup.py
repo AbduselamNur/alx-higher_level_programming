2d0a7f8549bed7e8baac10e8f111f9dc9fa50936
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-07-12 08:01:51 -0700

    first-commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module that list all avelable attribute and methode of an object
+"""
+
+
+def lookup(obj):
+    """
+    function that accept one arrgument
+    """
+    return dir(obj)
