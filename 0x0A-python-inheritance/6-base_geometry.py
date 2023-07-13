71b6d0124bf410693f10e867897c352acf9b4229
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-07-13 00:03:43 -0700

    raise- execption

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module that BaseGeometry Class
+"""
+
+
+class BaseGeometry:
+    """
+    class that is BaseGeometry
+    """
+    def area(self):
+        """
+        Raise:
+            Exception: area() is not implemented
+        """
+        raise Exception("area() is not implemented")
