d1488f32696339e6e17a69639a2a6efc1a818963
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-07-13 12:15:56 -0700

    int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""MyInt Module"""
+
+
+class MyInt(int):
+    """a class MyInt that inherits from int"""
+    def __init__(self, number):
+        self.number = number
+
+    def __ne__(self, value):
+        return (self.number == value)
+
+    def __eq__(self, value):
+        return (self.number != value)
+
+    def __str__(self):
+        return (str(self.number))
