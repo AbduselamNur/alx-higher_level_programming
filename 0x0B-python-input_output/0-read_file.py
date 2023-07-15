2b98d6981b6d30e5efcac8d532dc75a51f6aee66
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-07-14 22:02:35 -0700

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Read file Module"""
+
+
+def read_file(filename=""):
+    """
+    a function that reads a text file (UTF8) and prints it to stdout
+    Args:
+        filename: file that read
+    """
+    with open(filename, "r", encoding="utf-8") as myfile:
+        cont = myfile.read()
+    print(cont)
