1d138a7d34e18d28f43971b81043511bedefb2ac
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-07-17 01:20:30 -0700

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,15 +2,15 @@
 import sys
 
 
-def print_info():
+def p_info():
     print('File size: {:d}'.format(file_size))
 
-    for s, c in sorted(s.items()):
-        if c > 0:
-            print('{}: {:d}'.format(s, c))
+    for scode, code_times in sorted(status_codes.items()):
+        if code_times > 0:
+            print('{}: {:d}'.format(scode, code_times))
 
 
-s = {
+status_codes = {
     '200': 0,
     '301': 0,
     '400': 0,
@@ -21,21 +21,21 @@ s = {
     '500': 0
 }
 
-lc = 0
+c = 0
 file_size = 0
 
 try:
     for line in sys.stdin:
-        if lc != 0 and lc % 10 == 0:
-            print_info()
+        if c != 0 and c % 10 == 0:
+            p_info()
 
         pieces = line.split()
 
         try:
             status = int(pieces[-2])
 
-            if str(status) in s.keys():
-                s[str(status)] += 1
+            if str(status) in status_codes.keys():
+                status_codes[str(status)] += 1
         except:
             pass
 
@@ -44,9 +44,9 @@ try:
         except:
             pass
 
-        lc += 1
+        c += 1
 
-    print_info()
+    p_info()
 except KeyboardInterrupt:
-    print_info()
+    p_info()
     raise
