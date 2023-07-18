9d76ff5802ed4370e3efd07972e2076f8d6f9b03
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-07-18 00:28:10 -0700

    first rectangle tasting

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -7,23 +7,7 @@ from models.rectangle import Rectangle
 
 class TestReactangle(unittest.TestCase):
     def test_two_arg(self):
-        r = Rectangle(1, 2)
+        r_1 = Rectangle(1, 2)
+        r_2 = Rectangle(3, 4)
 
-        self.assertEqual(r.width, 1)
-        self.assertEqual(r.height, 2)
-        self.assertEqual(r.x, 0)
-        self.assertEqual(r.y, 0)
-
-    def test_thr_arg(self):
-        r2 = Rectangle(1, 2, 3)
-        self.assertEqual(r2.width, 1)
-        self.assertEqual(r2.height, 2)
-        self.assertEqual(r2.x, 3)
-        self.assertEqual(r2.y, 0)
-
-    def test_four_arg(self):
-        r2 = Rectangle(1, 2, 3, 4)
-        self.assertEqual(r2.width, 1)
-        self.assertEqual(r2.height, 2)
-        self.assertEqual(r2.x, 3)
-        self.assertEqual(r2.y, 4)
+        self.assertEqual(r_1.id, r_2.id - 1)
