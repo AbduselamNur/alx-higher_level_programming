3fc0fa3531903d300a4dde230c64a1f2c34bf9e2
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-07-18 08:31:49 -0700

    Display #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,10 +79,10 @@ class Rectangle(Base):
          prints in stdout the Rectangle
          instance with the character #
          """
-        for i in range(self.height):
-            for j in range(self.width):
-                print("#", end="")
+        for i in range(self.y):
             print()
+        for j in range(self.height):
+            print(" " * self.x + "#" * self.width)
 
     def __str__(self):
         string = "[Rectangle] ({}) {}/{} - {}/{}".format(
