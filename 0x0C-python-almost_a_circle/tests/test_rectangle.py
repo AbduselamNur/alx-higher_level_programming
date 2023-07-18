dd3b4c04d38d52b6e106503d784f3b40a85e3e53
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-07-17 23:53:42 -0700

    rectangle testing

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -6,11 +6,17 @@ from models.rectangle import Rectangle
 
 
 class TestReactangle(unittest.TestCase):
-    def test_init(self):
+    def test_two_arg(self):
         r = Rectangle(1, 2)
 
-        self.assertEqual(r.id, 2)
         self.assertEqual(r.width, 1)
         self.assertEqual(r.height, 2)
         self.assertEqual(r.x, 0)
         self.assertEqual(r.y, 0)
+
+    def test_3_arg(self):
+        r2 = Rectangle(1, 2, 3)
+        self.assertEqual(r2.width, 1)
+        self.assertEqual(r2.height, 2)
+        self.assertEqual(r2.x, 3)
+        self.assertEqual(r2.y, 0)
