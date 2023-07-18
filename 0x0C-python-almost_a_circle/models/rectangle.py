3d2b1c73a568211d70dd86d0002c513c475d68b5
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-07-17 22:10:01 -0700

    First Rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Module models/rectangle.py"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    the class Rectangle that inherits from Base
+    """
+
+    __width = None
+    __height = None
+    __x = None
+    __y = None
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def height(self, height):
+        self.__height = height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        self.__y = y
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Intialize the class"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
