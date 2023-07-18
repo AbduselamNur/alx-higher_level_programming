e786e1dd0bf26d6e96eaf73eb1eb6956868f8a4c
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-07-18 04:56:58 -0700

    testing

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -8,6 +8,8 @@ class test_base(unittest.TestCase):
     def test_None(self):
         b1 = Base()
         self.assertEqual(b1.id, 1)
+
     def test_not_none(self):
         b1 = Base(10)
         self.assertEqual(b1.id, 10)
+
