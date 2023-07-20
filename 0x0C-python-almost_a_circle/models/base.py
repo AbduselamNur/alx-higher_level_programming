cfd30a3f142dc87edfca21afdc519127eaac9abf
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-07-20 12:07:19 -0700

    advanced

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -76,6 +76,23 @@ class Base:
         except FileNotFoundError:
             return []
 
+     @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Write the CSV serialization
+        """
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w", newline="") as csvfile:
+            if list_objs is None or list_objs == []:
+                csvfile.write("[]")
+            else:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                for obj in list_objs:
+                    writer.writerow(obj.to_dictionary())
+
     @classmethod
     def load_from_file_csv(cls):
         """Return a list of classes instantiated from a CSV file.
