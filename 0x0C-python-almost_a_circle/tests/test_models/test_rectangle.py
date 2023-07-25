865b9e3d7f9b3db07285c1f14c0c0b571833f63d
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-07-25 17:47:29 +0300

    testing models

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import unittest
+from models.rectangle import Rectangle
+
+class TestRectangle(unittest.TestCase):
+    def setUp(self):
+        self.rect1 = Rectangle(1, 2)
+        self.rect2 = Rectangle(1, 2, 3)
+        self.rect3 = Rectangle(1, 2, 3, 4)
+
+    def test_attributes(self):
+        self.assertEqual(self.rect1.width, 1)
+        self.assertEqual(self.rect1.height, 2)
+        self.assertEqual(self.rect1.x, 0)
+        self.assertEqual(self.rect1.y, 0)
+        self.assertEqual(self.rect2.width, 1)
+        self.assertEqual(self.rect2.height, 2)
+        self.assertEqual(self.rect2.x, 3)
+        self.assertEqual(self.rect2.y, 0)
+        self.assertEqual(self.rect3.width, 1)
+        self.assertEqual(self.rect3.height, 2)
+        self.assertEqual(self.rect3.x, 3)
+        self.assertEqual(self.rect3.y, 4)
