6f0e088d91bed02195337d2b3cc18eea59e5bbf7
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-07-25 14:43:50 +0300

    testing models

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,9 +3,20 @@ import unittest
 from models.base import Base
 
 class TestBase(unittest.TestCase):
-    def test_id(self):
+    def test_no_arg(self):
         b1 = Base()
         b2 = Base()
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
+        self.assertEqual(b1.id, b2.id - 1)
 
+    def test_three_bases(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 2)
+    def test_None_id(self):
+        b1 = Base(None)
+        b2 = Base(None)
+        self.assertEqual(b1.id, b2.id - 1)
+
+    def test_unique_id(self):
+        self.assertEqual(12, Base(12).id)
