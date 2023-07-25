daaf3ce6a5acdd1c3d28e63f5460f17b18253a07
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-07-25 14:31:09 +0300

    testing models

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,4 +6,6 @@ class TestBase(unittest.TestCase):
     def test_id(self):
         b1 = Base()
         b2 = Base()
-        self.assertEqual(b1.id, b2.id - 1)
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+
