d921f049b89ec2663915f210dc7595decd58c7ad
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-07-25 08:07:29 +0300

    testing modules

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,8 @@
+import unittest
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    def test_id(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id -1)
