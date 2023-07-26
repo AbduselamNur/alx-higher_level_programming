02cc4178378444b86d5adbb2703ba366538e576f
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-07-26 22:27:51 +0300

    testing modules

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -51,22 +51,16 @@ class TestBase_to_json_string(unittest.TestCase):
         r = Rectangle(10, 7, 2, 8, 6)
         self.assertTrue(len(Base.to_json_string([r.to_dictionary()])) == 53)
 
-    def test_to_json_string_empty_list(self):
-        self.assertEqual("[]", Base.to_json_string([]))
-
-    def test_to_json_string_none(self):
-        self.assertEqual("[]", Base.to_json_string(None))
-
     def test_to_json_string_rectangle_two_dicts(self):
         r1 = Rectangle(2, 3, 5, 19, 2)
         r2 = Rectangle(4, 2, 4, 1, 12)
         list_dicts = [r1.to_dictionary(), r2.to_dictionary()]
         self.assertTrue(len(Base.to_json_string(list_dicts)) == 106)
-
+    
     def test_to_json_string_square_type(self):
         s = Square(10, 2, 3, 4)
         self.assertEqual(str, type(Base.to_json_string([s.to_dictionary()])))
-
+    
     def test_to_json_string_square_one_dict(self):
         s = Square(10, 2, 3, 4)
         self.assertTrue(len(Base.to_json_string([s.to_dictionary()])) == 39)
@@ -77,15 +71,12 @@ class TestBase_to_json_string(unittest.TestCase):
         list_dicts = [s1.to_dictionary(), s2.to_dictionary()]
         self.assertTrue(len(Base.to_json_string(list_dicts)) == 78)
 
-    def test_to_json_string_no_args(self):
+    def test_to_json_string_empty_list(self):
+        self.assertEqual("[]", Base.to_json_string([]))
+        self.assertEqual("[]", Base.to_json_string(None))
         with self.assertRaises(TypeError):
-            Base.to_json_string()
-
+           Base.to_json_string()
     def test_to_json_string_more_than_one_arg(self):
         with self.assertRaises(TypeError):
             Base.to_json_string([], 1)
-    def test_to_json_string(self):
-        self.assertEqual(Base.to_json_string([{'id': 12}]), '[{"id": 12}]')
 
-    def test_to_json_string_empty(self):
-        self.assertEqual(Base.to_json_string([]), '[]')
