6efdbf6cd3a96fd0e7226d8c7df839a707f813db
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-07-26 13:57:20 +0300

    testing square models

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -5,8 +5,15 @@ from models.square import Square
 class TestSquare(unittest.TestCase):
     def test_square_one_arg(self):
         s = Square(1)
-        self.assertEqual(s.width, 1)
+        self.assertEqual(s.size, 1)
 
     def test_square_two_arg(self):
         s = Square(1, 2)
         self.assertEqual(s.size, 1)
+        self.assertEqual(s.x, 2)
+
+    def test_square_three_arg(self):
+        s = Square(1, 2, 3)
+        self.assertEqual(s.size, 1)
+        self.assertEqual(s.x, 2)
+        self.assertEqual(s.y, 3)
