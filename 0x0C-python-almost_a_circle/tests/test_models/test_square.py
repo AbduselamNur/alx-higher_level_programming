c1a34c3da251f5d4ca1e359e65c59cba9a76c39a
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-07-26 13:51:18 +0300

    testing square models

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -3,6 +3,10 @@ import unittest
 from models.square import Square
 
 class TestSquare(unittest.TestCase):
-    def test_square(self):
+    def test_square_one_arg(self):
         s = Square(1)
         self.assertEqual(s.width, 1)
+
+    def test_square_two_arg(self):
+        s = Square(1, 2)
+        self.assertEqual(s.size, 1)
