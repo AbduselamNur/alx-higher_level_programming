7209c3f70d100e2f426925a9e7ae221f8ca3b79f
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-10-24 13:55:18 +0300

    count the character in films

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,9 +4,7 @@ const req = require('request');
 const url = process.argv[2];
 const searchUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
 req(url, (err, response, body) => {
-  if (err) {
-    console.error(err);
-  } else {
+  if (!err) {
     const data = JSON.parse(body);
     const count = data.results.filter((film) => {
       return film.characters.includes(searchUrl);
