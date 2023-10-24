61c58e368c633cf6a4a5d926c2eb61b2ebb45887
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-10-24 17:36:45 +0300

    print list of caractor

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,32 +1,20 @@
 #!/usr/bin/node
-const request = require('request');
 
-const movieId = process.argv[2];
+const req = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
-request.get(`https://swapi.dev/api/films/${movieId}`, (err, response, body) => {
+req(url, (err, response, body) => {
   if (err) {
     console.error(err);
-  } else {
-    const data = JSON.parse(body);
-    const characters = data.characters;
-    let count = 0;
-    const printCharacterName = () => {
-      if (count === characters.length) {
-        return;
+  }
+  const characters = JSON.parse(body).characters;
+
+  for (let i = 0; i < characters.length; ++i) {
+    req(characters[i], (err, response, body) => {
+      if (err) {
+        console.error(err);
       }
-      const characterUrl = characters[count];
-      request.get(characterUrl, (err, response, body) => {
-        if (err) {
-          console.error(err);
-        } else {
-          const characterData = JSON.parse(body);
-          const characterName = characterData.name;
-          console.log(characterName);
-          count++;
-          printCharacterName();
-        }
-      });
-    };
-    printCharacterName();
+      console.log(JSON.parse(body).name);
+    });
   }
 });
